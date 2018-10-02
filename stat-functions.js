b9885d47ab5059d40d6733f90933dc4d089b35ff
Philip B. Stark <stark@risklaw.org>
2018-10-01 17:13:51 -0700

    added statistical functions including chi-square percentile function

---
+++ stat-functions.js
@@ -0,0 +1,234 @@
+function chi2Cdf(df,  x) {
+    var p =  (df == Math.floor(df)) ? gammaCdf(x,df/2,2) : Number.NaN;
+    return p;
+}
+
+function chi2Pdf(df, x) {
+  if (x <= 0) {
+    return 0.0;
+  } else {
+    return gammaPdf(x, Math.floor(df)/2, 2);
+  }
+}
+
+function chi2Inv( p, df ) { // kluge for chi-square quantile function.
+    var tolAbs = 1.0e-8;
+    var tolRel = 1.0e-5;
+    var loP, hiP, currP, guess, guessLo, guessHi;
+    if (p == 0.0) {
+        guess = 0.0;
+    } else if ( p == 1.0 ) {
+        guess = Number.POSITIVE_INFINITY;
+    } else if ( p < 0.0 ) {
+        guess = Number.NaN;
+    } else {
+        guess = Math.max(0.0, df + Math.sqrt(2*df)*normInv(p)); // guess from normal approx
+        currP = chi2Cdf(df, guess);
+        loP = currP;
+        hiP = currP;
+        guessLo = guess;
+        guessHi = guess;
+        while (loP > p) { // step down
+            guessLo = 0.8*guessLo;
+            loP = chi2Cdf(df, guessLo);
+        }
+        while (hiP < p) { // step up
+            guessHi = 1.2*guessHi;
+            hiP = chi2Cdf(df, guessHi);
+        }
+        guess = (guessLo + guessHi)/2.0;
+        currP = chi2Cdf(df, guess);
+        while ( (Math.abs(currP - p) > tolAbs) || (Math.abs(currP - p)/p > tolRel) ) { // bisect
+            if ( currP < p ) {
+                guessLo = guess;
+            } else {
+                guessHi = guess;
+            }
+            guess = (guessLo + guessHi)/2.0;
+            currP = chi2Cdf(df, guess);
+        }
+    }
+    return guess;
+}
+
+function gammaCdf( x,  a,  b) { // gamma distribution CDF.
+    var p = Number.NaN;
+    if (a <= 0 || b <= 0) {
+    } else if (x <= 0) {
+        p = 0.0;
+    } else {
+        p = Math.min(incGamma(x/b, a), 1.0);
+    }
+    return p;
+}
+
+function incGamma( x,  a) {
+    var inc = 0;
+    var gam = lnGamma(a+rmin);
+    if (x === 0.0) {
+        inc = 0;
+    } else if (a === 0) {
+        inc = 1;
+    } else if (x < a+1) {
+        var ap = a;
+        var sum = 1.0/ap;
+        var del = sum;
+        while (Math.abs(del) >= 10*eps*Math.abs(sum)) {
+            del *= x/(++ap);
+            sum += del;
+        }
+        inc = sum * Math.exp(-x + a*Math.log(x) - gam);
+    } else if (x >= a+1) {
+       var a0 = 1;
+       var a1 = x;
+       var b0 = 0;
+       var b1 = 1;
+       var fac = 1;
+       var n = 1;
+       var g = 1;
+       var gold = 0;
+       var ana;
+       var anf;
+       while (Math.abs(g-gold) >= 10*eps*Math.abs(g)) {
+            gold = g;
+            ana = n - a;
+            a0 = (a1 + a0 *ana) * fac;
+            b0 = (b1 + b0 *ana) * fac;
+            anf = n*fac;
+            a1 = x * a0 + anf * a1;
+            b1 = x * b0 + anf * b1;
+            fac = 1.0 / a1;
+            g = b1 * fac;
+            n++;
+       }
+       inc = 1 - Math.exp(-x + a*Math.log(x) - gam) * g;
+    }
+    return inc;
+}
+
+function gammaPdf(x, a, b) {
+  var ans = Number.NaN;
+  if (a <= 0 || b <= 0) {
+    ans = Number.NaN;
+  } else if (x > 0) {
+    ans = Math.exp((a-1)*Math.log(x)-(x/b)-lnGamma(a)-a*Math.log(b));
+  } else if (x === 0 && a < 1) {
+    ans = Number.POSITIVE_INFINITY;
+  } else if (x === 0 && a == 1) {
+    ans = 1/b;
+  }
+  return ans;
+}
+
+function lnGamma(x) {
+/*  natural ln(gamma(x)) without computing gamma(x)
+    P.B. Stark
+
+      JavaScript subroutine is based on a MATLAB program by C. Moler,
+      in turn based on a FORTRAN program by W. J. Cody,
+      Argonne National Laboratory, NETLIB/SPECFUN, June 16, 1988.
+
+      References:
+
+      1) W. J. Cody and K. E. Hillstrom, 'Chebyshev Approximations for
+         the Natural Logarithm of the Gamma Function,' Math. Comp. 21,
+         1967, pp. 198-203.
+
+      2) K. E. Hillstrom, ANL/AMD Program ANLC366S, DGAMMA/DLGAMA, May,
+         1969.
+
+      3) Hart, Et. Al., Computer Approximations, Wiley and sons, New
+         York, 1968.
+*/
+
+     var d1 = -5.772156649015328605195174e-1;
+     var p1 = [4.945235359296727046734888e0, 2.018112620856775083915565e2,
+           2.290838373831346393026739e3, 1.131967205903380828685045e4,
+           2.855724635671635335736389e4, 3.848496228443793359990269e4,
+           2.637748787624195437963534e4, 7.225813979700288197698961e3];
+     var q1 = [6.748212550303777196073036e1, 1.113332393857199323513008e3,
+           7.738757056935398733233834e3, 2.763987074403340708898585e4,
+           5.499310206226157329794414e4, 6.161122180066002127833352e4,
+           3.635127591501940507276287e4, 8.785536302431013170870835e3];
+     var d2 = 4.227843350984671393993777e-1;
+     var p2 = [4.974607845568932035012064e0, 5.424138599891070494101986e2,
+           1.550693864978364947665077e4, 1.847932904445632425417223e5,
+           1.088204769468828767498470e6, 3.338152967987029735917223e6,
+           5.106661678927352456275255e6, 3.074109054850539556250927e6];
+     var q2 = [1.830328399370592604055942e2, 7.765049321445005871323047e3,
+           1.331903827966074194402448e5, 1.136705821321969608938755e6,
+           5.267964117437946917577538e6, 1.346701454311101692290052e7,
+           1.782736530353274213975932e7, 9.533095591844353613395747e6];
+     var d4 = 1.791759469228055000094023e0;
+     var p4 = [1.474502166059939948905062e4, 2.426813369486704502836312e6,
+           1.214755574045093227939592e8, 2.663432449630976949898078e9,
+           2.940378956634553899906876e10, 1.702665737765398868392998e11,
+           4.926125793377430887588120e11, 5.606251856223951465078242e11];
+     var q4 = [2.690530175870899333379843e3, 6.393885654300092398984238e5,
+           4.135599930241388052042842e7, 1.120872109616147941376570e9,
+           1.488613728678813811542398e10, 1.016803586272438228077304e11,
+           3.417476345507377132798597e11, 4.463158187419713286462081e11];
+     var c = [-1.910444077728e-03, 8.4171387781295e-04,
+          -5.952379913043012e-04, 7.93650793500350248e-04,
+          -2.777777777777681622553e-03, 8.333333333333333331554247e-02,
+           5.7083835261e-03];
+
+     var lng = Number.NaN;
+     var mach = 1.e-12;
+     var den = 1.0;
+     var num = 0;
+     var xm1, xm2, xm4;
+     var i = 0;
+
+   if (x < 0) {
+       return lng;
+   } else if (x <= mach) {
+       return -Math.log(x);
+   } else if (x <= 0.5) {
+      for (i = 0; i < 8; i++) {
+            num = num * x + p1[i];
+            den = den * x + q1[i];
+      }
+      lng = -Math.log(x) + (x * (d1 + x * (num/den)));
+   } else if (x <= 0.6796875) {
+      xm1 = x - 1.0;
+      for (i = 0; i < 8; i++) {
+         num = num * xm1 + p2[i];
+         den = den * xm1 + q2[i];
+      }
+      lng = -Math.log(x) + xm1 * (d2 + xm1*(num/den));
+   } else if (x <= 1.5) {
+      xm1 = x - 1.0;
+      for (i = 0; i < 8; i++) {
+         num = num*xm1 + p1[i];
+         den = den*xm1 + q1[i];
+      }
+      lng = xm1 * (d1 + xm1*(num/den));
+   } else if (x <= 4.0) {
+      xm2 = x - 2.0;
+      for (i = 0; i<8; i++) {
+         num = num*xm2 + p2[i];
+         den = den*xm2 + q2[i];
+      }
+      lng = xm2 * (d2 + xm2 * (num/den));
+   } else if (x <= 12) {
+      xm4 = x - 4.0;
+      den = -1.0;
+      for (i = 0; i < 8; i++)  {
+         num = num * xm4 + p4[i];
+         den = den * xm4 + q4[i];
+      }
+      lng = d4 + xm4 * (num/den);
+   } else {
+      var r = c[6];
+      var xsq = x * x;
+      for (i = 0; i < 6; i++) {
+         r = r / xsq + c[i];
+      }
+      r = r / x;
+      var lnx = Math.log(x);
+      var spi = 0.9189385332046727417803297;
+      lng = r + spi - 0.5*lnx + x*(lnx-1);
+    }
+    return lng;
+} // ends lnGamma
